4ce33d410f8b57634d027944ff0af2b96dced017
Illia Tishchenko <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-22 22:24:31 -0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,27 @@
+# car_prices = {'Mercedes': 5000, 'Audi': 4000, 'kia': 2000}
+# print(car_prices)
+
+# print(car_prices['Mercedes'])
+
+# car_prices['lt'] = 1000
+# print(car_prices)
+
+# car_prices['Mercedes'] = 10000;
+# print(car_prices)
+
+
+# del car_prices['Audi']
+# print(car_prices)
+
+person = {
+    'first name': 'Illia',
+    'last name': 'Tishchenko',
+    'age': 24,
+    'hobbies': ['Software Development', 'basketball'],
+    'children': {'no': 'son', 'no2': 'daughter'}
+}
+print(person['age'])
+
+print(person['hobbies'][0])
+
+print(person['children']['no2'])
